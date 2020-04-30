54983bb08318dc464adfe97f5d71fbb90ed1918c
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-30 12:39:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-    @analyzed_text = TextAnalyzer.new(text_from_user) #creating a instance of the TexAnalyzer classand assinging it to the instance variable @analyzed_text
+    #creating a instance of the TexAnalyzer classand assinging it to the instance variable @analyzed_text
+    @analyzed_text = TextAnalyzer.new(text_from_user) 
     erb :results
   end
 end
